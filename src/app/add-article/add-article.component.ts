bb055b799f51108ca84874aac0a17fc52d64c43f
Sailesh Patnala <sailesh.patnala.96@gmail.com>
2017-09-20 12:37:54 -0500

    completed feature - add featured articles

--- src/app/add-article/add-article.component.ts
+++ src/app/add-article/add-article.component.ts
@@ -14,22 +14,29 @@ import { News } from '../share/news.model';
 export class AddArticleComponent implements OnInit {
   public countries : string[] = [];
   public news : News = new News('', '', new Date(), [''], '', '', '', '');
+  public added : boolean = false;
 
   constructor( private countryService: CountryService,
   	private newsService: NewsService ) { }
 
-  public onSubmit() {
+  public onSubmit() : void {
   	let transformedNews = new News('', this.news.title, this.news.date, this.news.author, "ASUCD", this.news.text, this.news.image, this.news.country);
   	this.newsService.addNews(transformedNews).subscribe(
   		(res: Response) => {
   			console.log("added article");
+        this.added = true;
   		},
   		(error: any) => { alert(`Cannot add article!`) });
+  }
+
+  public refresh() : void {
+    window.location.reload();
   }
 
   ngOnInit() {
   	console.log('onInit fired!');
   	this.countries = this.countryService.getCountries();
+    this.added = false;
   }
 
 }
